1ae7b53afbaf4256b29d128ed086a0c21fdef868
Hemaa <Hemaa@HemaaBharath>
2026-04-24 19:33:06 +0530

    First time committing.

---
+++ src/test/java/demo/PractiseTest.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class PractiseTest {
+	
+	@Test(groups = "smoke")
+	public void appleTest() {
+		System.out.println("Apple Smart Watch");
+	}
+}
